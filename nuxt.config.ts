2a756b4cb0231d29d13134605dfac26d7b0b870d
Ariiq354 <realgoldcraft@gmail.com>
2024-09-25 17:39:05 +0700

    update database conn config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,15 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   future: {
     compatibilityVersion: 4,
   },
-  modules: ['@nuxt/ui', 'nuxt-security', '@nuxt/fonts'],
+  modules: ["@nuxt/ui", "nuxt-security", "@nuxt/fonts"],
   devtools: { enabled: true },
-
-  runtimeConfig: {
-    databaseUrl: process.env.DATABASE_URL,
-    databaseAuthToken: process.env.DATABASE_AUTH_TOKEN,
-  },
 
   security: {
     headers: {
       crossOriginEmbedderPolicy:
-        process.env.NODE_ENV === 'development' ? 'unsafe-none' : 'require-corp',
+        process.env.NODE_ENV === "development" ? "unsafe-none" : "require-corp",
     },
   },
 });
--- server/database/index.ts
+++ server/database/index.ts
@@ -1,14 +1,11 @@
-import { DrizzleSQLiteAdapter } from '@lucia-auth/adapter-drizzle';
-import { drizzle } from 'drizzle-orm/libsql';
-import { createClient } from '@libsql/client';
-import * as auth from './schema/auth';
-import * as ticketing from './schema/ticketing';
-const config = useRuntimeConfig();
+import { createClient } from "@libsql/client";
+import { DrizzleSQLiteAdapter } from "@lucia-auth/adapter-drizzle";
+import { drizzle } from "drizzle-orm/libsql";
+import * as auth from "./schema/auth";
+import * as ticketing from "./schema/ticketing";
 
 const client = createClient({
-  // url: 'file:test.db',
-  url: config.databaseUrl,
-  authToken: config.databaseAuthToken,
+  url: "file:test.db",
 });
 
 export const db = drizzle(client, {
@@ -18,4 +15,8 @@ export const db = drizzle(client, {
   },
 });
 
-export const adapter = new DrizzleSQLiteAdapter(db, auth.sessionTable, auth.userTable);
+export const adapter = new DrizzleSQLiteAdapter(
+  db,
+  auth.sessionTable,
+  auth.userTable
+);
